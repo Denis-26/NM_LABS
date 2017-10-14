ba9105e0b9a5510bdc0aa2e71080a067df4ee3b8
Denis-26 <kartavenkodenis0@gmail.com>
2017-10-15 01:18:51 +0300

    fix upper add lower

--- l1_hs/src/Horner.hs
+++ l1_hs/src/Horner.hs
@@ -1,12 +1,21 @@
 module Horner where
 
-upperBorder :: [Int] -> Int
-upperBorder coeffs = sum (takeWhile (\x -> not $ compare_coeffs $ horner coeffs x ) [0..])
+upperBorder :: [Int] -> Int -> Int
+upperBorder coeffs x
+    | compare_coeffs $ horner coeffs x = x
+    | otherwise = upperBorder coeffs (x + 1)
 
+lowerBorder :: [Int] -> Int -> Int
+lowerBorder coeffs x
+    | compare_coeffs $ horner normal_coeffs x = negate x
+    | otherwise = lowerBorder normal_coeffs (x + 1)
+    where normal_coeffs = transform coeffs
 
 horner :: [Int] -> Int -> [Int]
 horner coeffs point = tail $ scanl (\acc x -> acc * point + x) 0 coeffs
 
-
 compare_coeffs :: [Int] -> Bool
 compare_coeffs horner_coeffs = (head horner_coeffs) >= 0 && (all (>0) $ tail horner_coeffs)
+
+transform :: [Int] -> [Int]
+transform coeffs = [((-1)^i)*coeff | (i, coeff) <- zip [0..length coeffs] coeffs]
--- l1_hs/src/main.hs
+++ l1_hs/src/main.hs
@@ -1,4 +1,5 @@
 import qualified Horner as H
 
 main = do
-    print $ H.upperBorder [2, -4, 5, -2]
+    print $ H.upperBorder [2, -4, 5, -2] 1
+    print $ H.lowerBorder [2, -4, 5, -2] 1
