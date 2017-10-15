b7a54a37369b74384654f28ab4a866c400b23c11
Denis-26 <kartavenkodenis0@gmail.com>
2017-10-15 21:39:44 +0300

    LOVE HASKELL

--- l1_hs/src/RootSeparation.hs
+++ l1_hs/src/RootSeparation.hs
@@ -10,8 +10,7 @@ polinom_values_pairs :: [Int] -> (Int, Int) -> [(Int, Int)]
 polinom_values_pairs coeffs (x1, x2) = pairs [polinom_in_point coeffs x | x <- [x1..x2]]
 
 intervals :: [(Int, Int)] -> (Int, Int) -> [((Int, Int), (Int, Int))]
-intervals values (x1, x2) = filter (\( (x1, x2), (y1, y2) ) -> x1 * x2 < 0) ints
-    where ints = zip values $ pairs [x1..x2]
+intervals values (x1, x2) = filter (\( (x1, x2), (y1, y2) ) -> x1 * x2 < 0) $ zip values $ pairs [x1..x2]
 
 separate :: [Int] -> (Int, Int) -> [(Int, Int)]
 separate coeffs (x1, x2) =  map (\((x1, x2), (y1, y2)) -> (y1, y2)) $ intervals (polinom_values_pairs coeffs (x1, x2)) (x1, x2)
