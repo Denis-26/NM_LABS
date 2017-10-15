217c4dedf656d4794b193e99976f5a0e4a3d0233
Denis-26 <kartavenkodenis0@gmail.com>
2017-10-15 21:38:05 +0300

    FUCK HASKELL

--- l1_hs/src/main.hs
+++ l1_hs/src/main.hs
@@ -4,4 +4,4 @@ import qualified RootSeparation as R
 main = do
     print $ H.upperBorder [2, -4, 5, -2] 1
     print $ H.lowerBorder [2, -4, 5, -2] 1
-    print $ R.separate [1, 0, -6, 2] [3, 0, 1, 0] (-3, 3)
+    print $ R.separate [1, 0, -6, 2] (-3, 3)
