813316c7a09f1376c34ae3e64cd7c3ef3998e4b9
Denis-26 <kartavenkodenis0@gmail.com>
2017-11-27 23:39:11 +0300

    final version

--- l2/Zeidel.py
+++ l2/Zeidel.py
@@ -11,7 +11,7 @@ def seidel(A, b, eps):
             s1 = sum(A[i][j] * x_new[j] for j in range(i))
             s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
             x_new[i] = (b[i] - s1 - s2) / A[i][i]
-        converge = sqrt(sum((x_new[i] - x[i]) ** 2 for i in range(n))) <= eps
+        converge = sqrt(sum((x_new[i] - x[i])**2 for i in range(n))) <= eps
         x = x_new
 
     return x
--- l2/main.py
+++ l2/main.py
@@ -1,9 +1,10 @@
 from Zeidel import seidel
 from Choletsky import choletsky
+from check_time import check_time
 import numpy as np
 
 A = [
-    [5,      1,  -1,  2],
+    [5,      1,  1,  2],
     [0.5,    4,   2,   -1],
     [1,    0.2,  -6,   3],
     [1,   -0.5,  3,   -6]
@@ -11,18 +12,21 @@ A = [
 B = [11, -4, -22.4, -8]
 
 def main():
-    result_s = seidel(A, B, 0.00001)
-    result_c = choletsky(A, B)
+    time, result_s = check_time(seidel, A, B, 0.0001429)
+    print("Time of Seidel method: {}".format(time))
 
-    print("Seidel method")
-    for i, el in enumerate(result_s):
-        print('x{} = {}'.format(i+1, el))
-    print("Check result: {}\n".format(np.allclose(np.dot(A, result_s), B)))
+    time, result_c = check_time(choletsky, A, B)
+    print("Time of Choletsky method: {}".format(time))
 
     print("Choletsky method")
     for i, el in enumerate(result_c):
         print('x{} = {}'.format(i+1, el))
-    print("Check result: {}".format(np.allclose(np.dot(A, result_c), B)))
+    print("Подставим в исходную матрицу ответ и получим: ", np.dot(A, result_c), end='\n\n')
 
+    print("Seidel method")
+    for i, el in enumerate(result_s):
+        print('x{} = {}'.format(i+1, el))
+
+    print("Подставим в исходную матрицу ответ и получим: ", np.dot(A, result_s))
 if __name__ == '__main__':
     main()
